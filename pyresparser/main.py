248f574c78192e1294f22b4a71817826e697802e
blolya <mast-94@mail.ru>
2022-12-27 13:05:24 +0300

    Pyresparser: saving to excel

--- pyresparser/main.py
+++ pyresparser/main.py
@@ -1,8 +1,10 @@
-import pandas as pd
+import os
 import argparse
 import glob
+import json
 from pathlib import Path
 from pyresparser import ResumeParser
+import pandas as pd
 
 arg_parser = argparse.ArgumentParser(prog="Pyresparser", description="Parsing resumes using pyresparser library")
 arg_parser.add_argument("-id", "--input_dir", help="Input directory of resume files")
@@ -26,6 +28,45 @@ input_files = list( filter(lambda f: Path(f).suffix in exts, input_files) ) \
     if len(exts) > 0 \
     else input_files
 
+resumes = []
 for input_file in input_files:
-    data = ResumeParser(input_file).get_extracted_data()
-    print(data)
+    resume = ResumeParser(input_file).get_extracted_data()
+    
+    # removing keys with None value
+    # to avoid conditioning during parsing
+    for k in list(resume):
+        if resume[k] == None:
+            del resume[k]
+
+    input_path = Path(input_file)
+    output_file = os.path.join(output_dir, f"{input_path.stem}.json")
+    with open(output_file, "w") as f:
+        f.write(json.dumps(resume))
+        f.close()
+
+    resumes.append(resume)
+
+rows_list = []
+for resume in resumes:
+    name = resume.get("name", "No name")
+    email = resume.get("email", "No email")
+    phone = resume.get("mobile_number", "No phone")
+    skills = "; ".join( resume.get("skills", []) )
+
+    college_name = resume.get("college_name", "")
+    degree = "; ".join( resume.get("degree", {}) )
+    education = f"{college_name}: {degree}"
+
+    designation = "; ".join(resume.get("designation", []))
+    experience = "; ".join(resume.get("experience", []))
+
+    companies = "; ".join(resume.get("company_names", []))
+
+    rows_list.append({
+        "name": name, "phone": phone, "email": email, "skills": skills,
+        "education": education, "profession": designation, "work_experience": experience,
+        "companies": companies
+    })
+
+df = pd.DataFrame(rows_list)
+df.to_excel(output_dir + "/resumes.xlsx")
